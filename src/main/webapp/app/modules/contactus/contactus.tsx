30a95e9679c562c7eae326e8899cbf5073748e2c
Ayoub METWALLI <Ayoub.metwalli@4d.com>
2021-06-17 10:29:21 +0100

    add contact us page

--- src/main/webapp/app/modules/contactus/contactus.tsx
+++ src/main/webapp/app/modules/contactus/contactus.tsx
@@ -1,11 +1,64 @@
 import './contactus.scss';
 import React from 'react';
 import { Alert } from 'reactstrap';
+import { Col, Row, Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { AvForm, AvField } from 'availity-reactstrap-validation';
 
 const ContactUs = () => {
+  const handleValidSubmit = (event, values) => {
+    //
+  };
+
   return (
     <Alert color="light">
-      <h5 className="text-center ">SOON! ....</h5>
+      <h3 className="text-center">يسعدنا دائمًا استقبال استفساراتكم</h3>
+      <AvForm className="section-form" id="contact-form" onValidSubmit={handleValidSubmit}>
+        <Row form>
+          <Col md={6}>
+            <AvField
+              className="input-form"
+              name="username"
+              label="الإسم"
+              placeholder={'الإسم'}
+              type="text"
+              validate={{
+                required: { value: true, errorMessage: 'يرجى كتابة اسمك' },
+              }}
+              data-cy="username"
+            />
+          </Col>
+          <Col md={6}>
+            <AvField
+              errorMessage="صيغة الايميل غير صحيحه"
+              className="input-form"
+              name="email"
+              label="البريد الإلكتروني"
+              placeholder={'البريد الإلكتروني'}
+              type="email"
+              validate={{
+                required: { value: true, errorMessage: 'يرجى كتابة الايميل الخاص بك' },
+              }}
+              data-cy="email"
+            />
+          </Col>
+        </Row>
+        <AvField
+          className="input-form"
+          name="message"
+          label="نص الرسالة"
+          placeholder={'نص الرسالة'}
+          type="textarea"
+          validate={{
+            required: { value: true, errorMessage: 'يرجى كتابة الرسالة' },
+          }}
+          data-cy="message"
+        />
+        <FormGroup className="text-center">
+          <Button size="lg" type="submit" data-cy="submit">
+            إرســـــال
+          </Button>
+        </FormGroup>
+      </AvForm>
     </Alert>
   );
 };
